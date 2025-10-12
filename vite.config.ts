9bf3692ff1a6bd835c7c24bff56ad719e4a0ba86
VernerAnton <105201748+VernerAnton@users.noreply.github.com>
2025-10-12 08:09:26 +0000

    Update build config to use docs folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // This is the line to add. It tells the app its final address.
-  base: '/subtitle2docx/', 
-  plugins: [react()],
+  // ...any other plugins or settings
+
+  build: {
+    outDir: 'docs' // Change the output folder to 'docs'
+  },
+  base: '/subtitle2docx/', // Set the correct base path
 })
